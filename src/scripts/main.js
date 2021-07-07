c1e136fdb40555156d35f7625045eb03c798cace
Dima Batiuk <batiuk20@gmail.com>
2021-07-07 15:54:47 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,31 @@ burger.addEventListener('click', () => {
   nav.classList.toggle('active');
   burger.classList.toggle('active');
 });
+
+new Swiper('.swiper-container', {
+  loop: true,
+  loopedSlides: 3,
+  slidesPerView: 2,
+
+  breakpoints: {
+    320: {
+      initialSlide: 1,
+      spaceBetween: 40,
+      slidesPerView: 1,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+      },
+    },
+
+    1100: {
+      slidesPerView: 2,
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    },
+  },
+});
